90738100fb6a899e124ba142d1ab38623238f3a4
AnnaBerezinka <annaberezina1111@gmail.com>
2026-08-14 20:06:05 +0300

    Update firebase.js to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARpZbWuYbW8cxHj4w-24Zwi-LLhk4taZE",
-  authDomain: "liniya-kds.firebaseapp.com",
-  projectId: "liniya-kds",
-  storageBucket: "liniya-kds.firebasestorage.app",
-  messagingSenderId: "В837329386469",
-  appId: "1:837329386469:web:ad98ff8d5108d03fb6cd0c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
